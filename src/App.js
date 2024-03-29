ee75bc97b9f442449ac92e1e38456e5e99b13fce
Sudhanshu <sudhanshu007@bhu.ac.in>
2024-03-29 18:57:18 +0530

    styled header

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Main from './components/Main';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import FormSection from './components/FormSection';
+import Counter from './components/Counter';
 function App() {
   return (
     <div className="App">
       <Header/>
       <Main/>
       <FormSection />
+      <Counter/>
       <Footer/>
     </div>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import './Main.css';
-import Counter from './Counter.js';
 import ControlledCarousel from './ControlledCarousel.js';
 const Main = () => {
   return (
     <div className="row">
       <div className="col-12 carousel">
         <ControlledCarousel/>
-      </div>
-      <div className="col-12 counterdiv">
-            <Counter/>
       </div>
     </div>
   )
