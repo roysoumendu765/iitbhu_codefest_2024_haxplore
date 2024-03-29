173368fb08e800e126ce29dc0326ad50c4e3147f
Sudhanshu <sudhanshu007@bhu.ac.in>
2024-03-29 22:19:39 +0530

    css configured

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,18 +2,22 @@ import React from 'react';
 import './Main.css';
 import ControlledCarousel from './ControlledCarousel.js';
 import Ticketpage from './Ticketpage.js';
+import Counter from './Counter.js';
 
 const Main = () => {
-  return (
-    <div className='row'>
-      <div className='col-12 carousel'>
-        <ControlledCarousel/>
-      </div>
-      <div className='col-12 ticketpage'>
-            <Ticketpage/>
-      </div>
-    </div>
-  )
+    return (
+        <div className='row'>
+            <div className='col-12 carousel'>
+                <ControlledCarousel />
+            </div>
+                <div className="counter">
+                    <Counter />
+                </div>
+                <div className='col-12 ticketpage'>
+                    <Ticketpage />
+                </div>
+        </div>
+    )
 }
 
 export default Main;
--- src/components/MainApp.js
+++ src/components/MainApp.js
@@ -1,19 +1,11 @@
 import React from "react";
 import Main from "./Main";
-// import Header from "./Header";
-// import  Footer from "./Footer";
-import Counter from "./Counter";
 export default function MainApp() {
     return (
         <div className="App">
-            
             <div className="main">
                 <Main />
             </div>
-            <div className="counter">
-                <Counter />
-            </div>
-
         </div>
     );
 }
