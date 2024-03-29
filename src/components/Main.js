fb13d12764748d9d721dbb2c9dc7dfc7c947d289
Sudhanshu <sudhanshu007@bhu.ac.in>
2024-03-29 17:31:20 +0530

    carousel added

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Main.css';
 import Counter from './Counter.js';
+import ControlledCarousel from './ControlledCarousel.js';
 const Main = () => {
   return (
     <div class="row">
       <div class="col-12 carousel">
-    
+        <ControlledCarousel/>
       </div>
       <div class="col-12 counterdiv">
             <Counter/>
