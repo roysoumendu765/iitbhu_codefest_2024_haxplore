06057590a92aeaaed81296ea6483609dc3fc097b
Sudhanshu <sudhanshu007@bhu.ac.in>
2024-03-29 16:09:00 +0530

    addedd form section for visitor details ..css remaining

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Main from './components/Main';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
+import FormSection from './components/FormSection';
 function App() {
   return (
     <div className="App">
       <Header/>
       <Main/>
+      <FormSection />
       <Footer/>
     </div>
   );
