da9bb1aa4083eda80dfa1b87e9807e20c40ce150
Rajeshpandit55 <sonuraj89033@gmail.com>
2024-03-29 16:29:19 +0530

    footer part html completed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,124 @@
+import { CgMail } from "react-icons/cg";
+import { IoMdContact } from "react-icons/io";
+import { CgWebsite } from "react-icons/cg";
+import { TiMessages } from "react-icons/ti";
+
+// import bhulogo from "./assests/bhulogo.png";
+
 import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+      <div>
+
+        {/* main title and logo */}
+
+        <div>
+          {/* <img src={bhulogo}></img> */}
+          <h2>Footer Temple</h2>
+        </div>
+
+        {/* Contact info Area */}
+
+        <div>
+          <h3>Contact info</h3>
+          <div>
+
+            <div>
+              <p><IoMdContact /></p>
+              <a href='#'> Contact Us</a>
+            </div>
+
+            <div>
+              <p><CgWebsite /></p>
+              <a href='#'> info@KashiVishwanth.com</a>
+            </div>
+
+            <div>
+              <p><TiMessages /></p>
+              <a href='#'> Send Your Enquery</a>
+            </div>
+
+          </div>
+
+        </div>
+
+        {/* Media Link Area */}
+
+        <div>
+          <h3>Media Links</h3>
+          <div>
+
+            <div>
+
+              <a href='#'>Temple Services</a>
+            </div>
+
+            <div>
+
+              <a href='#'>Login/Register</a>
+            </div>
+
+          </div>
+
+        </div>
+
+        {/* Policy Info */}
+
+        <div>
+          <h3>Policy Info</h3>
+          <div>
+
+            <div>
+              <a href='#'> How we Work</a>
+            </div>
+
+            <div>
+              <a href='#'> Privacy Policy</a>
+            </div>
+
+            <div>
+              <a href='#'> Term of Use</a>
+            </div>
+
+            <div>
+              <a href='#'> FAQ</a>
+            </div>
+
+            <div>
+              <a href='#'> Cancellation & Refund Policy</a>
+            </div>
+
+          </div>
+
+        </div>
+
+        {/* Payment gatyway link */}
+
+        <div>
+
+          <h3>Payment Partner</h3>
+
+          <div>
+            <img src="#"/>
+          </div>
+
+          <div>
+            <img src="#"/>
+          </div>
+
+          <div>
+            <img src="#"/>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
   )
 }
+
 
 export default Footer;
