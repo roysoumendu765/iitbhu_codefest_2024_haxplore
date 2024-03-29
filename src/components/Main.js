429a96089f626d5631b8e4c7f36f0cc105d73bfa
GauravSolo <gauravsharma9339@gmail.com>
2024-03-29 16:02:19 +0530

    added functionality of increasing counter

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Main.css';
-
+import Counter from './Counter.js';
 const Main = () => {
   return (
     <div class="row">
@@ -8,26 +8,7 @@ const Main = () => {
         
       </div>
       <div class="col-12 counterdiv">
-            <div class="row">
-              <div class="col-6 d-flex justify-content-center" >
-                <div class="column">
-                  <div class="card" style={{ minWidth: '200px', color: 'orange', border: '0'}}>
-                    <p><i class="fa fa-user"></i></p>
-                    <h3 class="fw-bold">200+</h3>
-                    <p class="fs-4">Visitors</p>
-                  </div>
-                </div>
-              </div>
-              <div class="col-6 d-flex justify-content-center">
-              <div class="column" >
-                  <div class="card" style={{ minWidth: '200px', color: 'orange', border: '0'}}>
-                    <p><i class="fa fa-user"></i></p>
-                    <h3 class="fw-bold">11+</h3>
-                    <p class="fs-4">Temples</p>
-                  </div>
-                </div>
-              </div>
-            </div>
+            <Counter/>
       </div>
     </div>
   )
