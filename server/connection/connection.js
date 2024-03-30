eef17df282aab13687cd9b0dfd8f21f77fc6fe00
Soumendu Roy <roysoumendu765@gmail.com>
2024-03-30 15:54:39 +0530

    added new connection string for mongodb

--- server/connection/connection.js
+++ server/connection/connection.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
-const password = encodeURIComponent("mybz?MWqr9c$HxF");
-const uri = `mongodb+srv://roysoumendu765:${password}@cluster0.jpoyoge.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const password = "abcdefgh";
+const uri = `mongodb+srv://abcdefgh:${password}@cluster0.jpoyoge.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 const connect = async () => {
     try {
-        await mongoose.connect(uri,{
-            serverSelectionTimeoutMS: 5000
-        });
+        await mongoose.connect(uri);
         console.log("DataBase Connection Established");
     } catch (error) {
         console.log(error)
