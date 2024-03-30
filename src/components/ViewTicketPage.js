543b4f3f8cb9c90b63dd3c4a5369788d9b1b9f8c
Sudhanshu <sudhanshu007@bhu.ac.in>
2024-03-31 02:33:55 +0530

    added sweet alert to the view ticket section

--- src/components/ViewTicketPage.js
+++ src/components/ViewTicketPage.js
@@ -1,19 +1,36 @@
 import React, { useState } from "react";
 import './ViewTicketPage.css';
 import List from "./List";
-const ViewTicketPage = () => {
+import Swal from 'sweetalert2';
 
+
+
+const ViewTicketPage = () => {
     const [listStatus, setListStatus] = useState(false);
     const [idNo, setIdNo] = useState('');
     const clickhandler = () => {
         // console.log("hello");
-        idNo===''?console.log("empty"):setListStatus(true);
+        idNo === '' ? sweetError() : setListStatus(true);
     }
-    const changeHandler=(e)=>{
+    const sweetError = () => {
+        Swal.fire({
+            title: "Error",
+            text: "ID Number can't be empty...",
+            icon: "error"
+        })
+    }
+    const sweetSuccess=()=>{
+    Swal.fire({
+        title: "Success",
+        text: "Ticket Generated SuccessFully.",
+        icon: "success"
+    })
+    }
+    const changeHandler = (e) => {
         setIdNo(e.target.value);
         // console.log(idNo);
     }
-    const refresh=()=>{
+    const refresh = () => {
         setListStatus(false);
         setIdNo('');
     }
@@ -32,7 +49,7 @@ const ViewTicketPage = () => {
                 </div>
                 <div className="inputContainerV">
                     <label htmlFor="">ID Number: &nbsp;</label>
-                    <input type="text" value={idNo} onChange={changeHandler}/>
+                    <input type="text" value={idNo} onChange={changeHandler} />
                 </div>
                 <div className="submitButton">
                     <button className="neumorphic-button" onClick={clickhandler}>
@@ -41,7 +58,10 @@ const ViewTicketPage = () => {
                 </div>
             </div>
             {
-                listStatus && <List idnum={idNo}/>
+                listStatus && <List idnum={idNo} />
+            }
+            {
+                listStatus && sweetSuccess()
             }
             {
                 listStatus && <button className="neumorphic-button" onClick={refresh}>Refresh</button>
