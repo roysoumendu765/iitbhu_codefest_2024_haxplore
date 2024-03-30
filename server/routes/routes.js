d27ebf0998656f93cf44ba274f0bc0b07d1ef106
Soumendu Roy <roysoumendu765@gmail.com>
2024-03-30 21:05:25 +0530

    updated routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -48,7 +48,7 @@ const updateStatus = (objId) => {
 router.get('/getAll', async (req,res) => {
     try {
         const data = await Model.find();
-        res.status(200).json();
+        res.status(200).json(data);
     } catch (error) {
         res.status(500).json({message: error.message})
     }
