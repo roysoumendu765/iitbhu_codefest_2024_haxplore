1becba6485d5c703f2dbe4a1ffd6954626eddfc2
GauravSolo <gauravsharma9339@gmail.com>
2024-03-31 23:59:31 +0530

    change in header

--- src/components/MainApp.js
+++ src/components/MainApp.js
@@ -3,8 +3,8 @@ import Main from "./Main";
 import CardSection from './CardSection';
 export default function MainApp() {
     return (
-        <div className="App">
-            <div className="main">
+        <div className="App w-100">
+            <div className="main w-100">
                 <Main />
                 <CardSection />
             </div>
