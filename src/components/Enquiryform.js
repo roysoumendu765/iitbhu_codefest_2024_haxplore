6fb036c00481e986d01f20b83338ca2fdad541e4
Sudhanshu <sudhanshu007@bhu.ac.in>
2024-03-31 13:10:36 +0530

    enquiry form reviewed

--- src/components/Enquiryform.js
+++ src/components/Enquiryform.js
@@ -2,10 +2,10 @@ import React from 'react'
 import './Enquiryform.css';
 
 const Enquiryform = () => {
-    return (    
+    return (
         <div className=' enguiry-container  mb-2 d-flex justify-content-center'>
             <div className='form-container w-50 mt-2 p-5 neumorphic-input rounded-2'>
-                <h2 className='text-center text-decoration-underline'>Asked Your Query</h2>
+                <h2 className='text-center'>Contact us...</h2>
                 <form>
                     <div class="form-group">
                         <label className='mt-3 neumorphic-label-enquiry'>Name</label>
@@ -13,15 +13,15 @@ const Enquiryform = () => {
                     </div>
                     <div class="form-group">
                         <label className='mt-3 neumorphic-label-enquiry' >Email address</label>
-                        <input  type="email" className="form-control neumorphic-input-enguiry mt-1" placeholder="name@example.com" />
+                        <input type="email" className="form-control neumorphic-input-enguiry mt-1" placeholder="name@example.com" />
                     </div>
                     <div class="form-group">
                         <label className='mt-3 neumorphic-label-enquiry' >Write your query here !</label>
                         <textarea class="neumorphic-input-enguiry form-control mt-1" rows="5"></textarea>
                     </div>
                     <div className="d-flex justify-content-center  mt-2">
-                    <button class="btn btn-primary ps-3 pe-3 " type="submit">submit</button>
-
+                        <button class="neumorphic-button" type="submit">submit</button>
+                        {/* btn btn-primary ps-3 pe-3  */}
                     </div>
                 </form>
             </div>
