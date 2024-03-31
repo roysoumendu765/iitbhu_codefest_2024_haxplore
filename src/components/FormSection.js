06d6e264d0aaa532f19fd155bd1478db354b8e9f
Sudhanshu <sudhanshu007@bhu.ac.in>
2024-03-31 19:23:54 +0530

    validation in progress

--- src/components/FormSection.js
+++ src/components/FormSection.js
@@ -19,6 +19,29 @@ export default function FormSection() {
     const [total, setTotal] = useState(0);
     const [waiting, setWaiting] = useState('calculating...');
 
+    // validating form field
+    const [formErrors, setFormErrors] = useState({});
+    const [isSubmit, setIsSubmit] = useState(false);
+
+    const validate = (values) => {
+        const errors = {};
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+        if (!values.username) {
+            errors.username = "Username is required!";
+        }
+        if (!values.email) {
+            errors.email = "Email is required!";
+        } else if (!regex.test(values.email)) {
+            errors.email = "This is not a valid email format!";
+        }
+        if (!values.mobilenum) {
+            errors.mobilenum = "Mobile Number is required";
+        } else if(values.mobilenum.length < 10) {
+            errors.mobilenum = "Mobile Number must have 10 characters";
+        }
+        return errors;
+    };
+
     const handlename = (e) => {
         setName(e.target.value);
         // waitingTime();
@@ -83,16 +106,16 @@ export default function FormSection() {
             "idnum": idnum,
             "no_of_person": noOfPersons
         }
-        console.log(data);
-        if (username.length <= 4) {
-            alert('Please enter a valid name');
-            setName('');
-            return;
-        } else if (age <= 4) {
-            alert("Invalid Age");
-            setAge('');
-            return;
-        }
+        // console.log(data);
+        // if (username.length <= 4) {
+        //     alert('Please enter a valid name');
+        //     setName('');
+        //     return;
+        // } else if (age <= 4) {
+        //     alert("Invalid Age");
+        //     setAge('');
+        //     return;
+        // }
         axios.post(`http://localhost:8000/booking/book`, { username, gender, age, email, mobilenum, idname, idnum, noOfPersons })
             .then(res => {
                 console.log(res.data.id);
@@ -218,82 +241,82 @@ export default function FormSection() {
     }, []);
 
     return (
-            <div className="container1 d-flex flex-column" >
-                    <legend className="heading text-center waiting-time py-2">Approx Waiting Time: {waiting}</legend>
-                <div className="col-12 col-md-6 col-lg-6 formContainer my-4">
-                    <fieldset className="neumorphic-fieldset">
-                        <legend className="d-flex justify-content-center heading text-center mb-3 w-100">Personal Details</legend>
-                        <form className="form-section">
-                            <div className="row ">
-                                <label htmlFor="name" required className="neumorphic-label col-sm-4 col-form-label">Name: &nbsp;</label>
-                                <div className="col-sm-8">
-                                    <input className="form-control nameInput neumorphic-input" placeholder="Enter Your Name" type="text" id="name" minLength="4" value={username} onChange={handlename} required /><br />
-                                </div>
+        <div className="container1 d-flex flex-column" >
+            <legend className="heading text-center waiting-time py-2">Approx Waiting Time: {waiting}</legend>
+            <div className="col-12 col-md-6 col-lg-6 formContainer my-4">
+                <fieldset className="neumorphic-fieldset">
+                    <legend className="d-flex justify-content-center heading text-center mb-3 w-100">Personal Details</legend>
+                    <form className="form-section">
+                        <div className="row ">
+                            <label htmlFor="name" required className="neumorphic-label col-sm-4 col-form-label">Name: &nbsp;</label>
+                            <div className="col-sm-8">
+                                <input className="form-control nameInput neumorphic-input" placeholder="Enter Your Name" type="text" id="name" minLength="4" value={username} onChange={handlename} required /><br />
                             </div>
-                            <div className="row ">
-                                <label htmlFor="gender" required className="neumorphic-label col-sm-4 col-form-label">Gender: &nbsp;</label>
-                                <div className="col-sm-8 d-flex align-items-center">
-                                    <input className="gendermale me-2 w-auto" type="radio" name="gender" value="male" onChange={handlegender} /> Male
-                                    <input className="genderfemale ms-4 me-2 w-auto" type="radio" name="gender" value="female" onChange={handlegender} /> Female
-                                    <input className="ms-4 me-2 w-auto" type="radio" name="gender" value="Others" onChange={handlegender} /> Others&nbsp;<br />
-                                </div>
+                        </div>
+                        <div className="row ">
+                            <label htmlFor="gender" required className="neumorphic-label col-sm-4 col-form-label">Gender: &nbsp;</label>
+                            <div className="col-sm-8 d-flex align-items-center">
+                                <input className="gendermale me-2 w-auto" type="radio" name="gender" value="male" onChange={handlegender} /> Male
+                                <input className="genderfemale ms-4 me-2 w-auto" type="radio" name="gender" value="female" onChange={handlegender} /> Female
+                                <input className="ms-4 me-2 w-auto" type="radio" name="gender" value="Others" onChange={handlegender} /> Others&nbsp;<br />
                             </div>
-                            <div className="row mb-1">
-                                <label htmlFor="age" required className="neumorphic-label col-sm-4 col-form-label">Age: &nbsp;</label>
-                                <div className="col-sm-8">
-                                    <input className="form-control ageInput neumorphic-input" type="number" name="age" id="age" value={age} onChange={handleAge} /><br />
-                                </div>
+                        </div>
+                        <div className="row mb-1">
+                            <label htmlFor="age" required className="neumorphic-label col-sm-4 col-form-label">Age: &nbsp;</label>
+                            <div className="col-sm-8">
+                                <input className="form-control ageInput neumorphic-input" type="number" name="age" id="age" value={age} onChange={handleAge} /><br />
                             </div>
-                            <div className="row mb-1">
-                                <label htmlFor="mobile" required className="neumorphic-label col-sm-4 col-form-label">Mobile: &nbsp;</label>
-                                <div className="col-sm-8">
-                                    <input className="form-control mobileInput neumorphic-input" placeholder="(+91) 0000000000" type="number" name="amobilege" id="mobile" maxLength="10" minLength="10" value={mobilenum} onChange={handleMobile} /><br />
-                                </div>
+                        </div>
+                        <div className="row mb-1">
+                            <label htmlFor="mobile" required className="neumorphic-label col-sm-4 col-form-label">Mobile: &nbsp;</label>
+                            <div className="col-sm-8">
+                                <input className="form-control mobileInput neumorphic-input" placeholder="(+91) 0000000000" type="number" name="amobilege" id="mobile" maxLength="10" minLength="10" value={mobilenum} onChange={handleMobile} /><br />
                             </div>
-                            <div className="row mb-1">
-                                <label htmlFor="id-name" required className="neumorphic-label col-sm-4 col-form-label">Id Name: &nbsp;</label>
-                                <div className="col-sm-8">
-                                    <select className="form-control idInput neumorphic-input" id="id-name" value={idname} onChange={handleIdName}>
-                                        <option value="aadharCard">Aadhar Card</option>
-                                        <option value="panCard">Pan Card</option>
-                                        <option value="other">Voter Card</option>
-                                        <option value="other">Passport</option>
-                                        <option value="other">Others</option>
-                                    </select>
-                                </div>
+                        </div>
+                        <div className="row mb-1">
+                            <label htmlFor="id-name" required className="neumorphic-label col-sm-4 col-form-label">Id Name: &nbsp;</label>
+                            <div className="col-sm-8">
+                                <select className="form-control idInput neumorphic-input" id="id-name" value={idname} onChange={handleIdName}>
+                                    <option value="aadharCard">Aadhar Card</option>
+                                    <option value="panCard">Pan Card</option>
+                                    <option value="other">Voter Card</option>
+                                    <option value="other">Passport</option>
+                                    <option value="other">Others</option>
+                                </select>
                             </div>
-                            <div className="row mb-1">
-                                <label htmlFor="id-no" required className="neumorphic-label col-sm-4 col-form-label">Id Number: &nbsp;</label>
-                                <div className="col-sm-8">
-                                    <input className="form-control idnumber-input neumorphic-input" type="text" id="id-no" value={idnum} onChange={handleIdNum} /><br />
-                                </div>
+                        </div>
+                        <div className="row mb-1">
+                            <label htmlFor="id-no" required className="neumorphic-label col-sm-4 col-form-label">Id Number: &nbsp;</label>
+                            <div className="col-sm-8">
+                                <input className="form-control idnumber-input neumorphic-input" type="text" id="id-no" value={idnum} onChange={handleIdNum} /><br />
                             </div>
-                            <div className="row mb-1">
-                                <label htmlFor="noOfPerson" required className="neumorphic-label col-sm-4 col-form-label">No. Of Persons: &nbsp;</label>
-                                <div className="col-sm-8">
-                                    <input className="form-control numberofperson-input neumorphic-input" min={0} type="number" id="noOfPerson" value={noOfPersons} onChange={handlePersons} /><br />
-                                </div>
+                        </div>
+                        <div className="row mb-1">
+                            <label htmlFor="noOfPerson" required className="neumorphic-label col-sm-4 col-form-label">No. Of Persons: &nbsp;</label>
+                            <div className="col-sm-8">
+                                <input className="form-control numberofperson-input neumorphic-input" min={0} type="number" id="noOfPerson" value={noOfPersons} onChange={handlePersons} /><br />
                             </div>
-                            <div className="row mb-1">
-                                <label htmlFor="email" required className="neumorphic-label col-sm-4 col-form-label">Email Address: &nbsp;</label>
-                                <div className="col-sm-8">
-                                    <input className="form-control emailInput neumorphic-input" placeholder="abcd@gmail.com" type="email" name="email" id="email" data-validate="email" value={email} onChange={handleEmail} /><br />                </div>
+                        </div>
+                        <div className="row mb-1">
+                            <label htmlFor="email" required className="neumorphic-label col-sm-4 col-form-label">Email Address: &nbsp;</label>
+                            <div className="col-sm-8">
+                                <input className="form-control emailInput neumorphic-input" placeholder="abcd@gmail.com" type="email" name="email" id="email" data-validate="email" value={email} onChange={handleEmail} /><br />                </div>
+                        </div>
+                        <div className="row mb-1">
+                            <label htmlFor="total" required className="neumorphic-label col-sm-4 col-form-label">Total: (in Rupees)</label>
+                            <div className="col-sm-8">
+                                <input className="form-control total neumorphic-input" type="number" name="total" id="total" value={total} disabled /><br />
                             </div>
-                            <div className="row mb-1">
-                                <label htmlFor="total" required className="neumorphic-label col-sm-4 col-form-label">Total: (in Rupees)</label>
-                                <div className="col-sm-8">
-                                    <input className="form-control total neumorphic-input" type="number" name="total" id="total" value={total} disabled /><br />
-                                </div>
+                        </div>
+                        <div className="row mb-1">
+                            <div className="col-12 d-flex justify-content-around">
+                                <button className="submitbtn neumorphic-button" type="submit" onClick={sendData}>Buy</button>
+                                <button className="Resetbtn neumorphic-button" onClick={clearForm}>Reset</button>
                             </div>
-                            <div className="row mb-1">
-                                <div className="col-12 d-flex justify-content-around">
-                                    <button className="submitbtn neumorphic-button" type="submit" onClick={sendData}>Buy</button>
-                                    <button className="Resetbtn neumorphic-button" onClick={clearForm}>Reset</button>
-                                </div>
-                            </div>
-                        </form>
-                    </fieldset>
-                </div>
+                        </div>
+                    </form>
+                </fieldset>
             </div>
+        </div>
     );
 }
